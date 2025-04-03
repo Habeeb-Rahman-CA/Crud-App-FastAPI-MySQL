3d24362d2b0e8ec10184eda4083d1abfa9d99ec8
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-04-03 11:09:40 +0530

    Implemented blog app logic

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = ""
+URL_DATABASE = "mysql+pymysql://root:test1234!@localhost:3306/BlogApp"
 
 engine = create_engine(URL_DATABASE)
 
